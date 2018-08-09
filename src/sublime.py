9821d441942a7adce9b782627c1c0ba76c267979
David Nicolson <david.nicolson@gmail.com>
2018-08-09 21:58:11 +0200

    :cop:

--- src/sublime.py
+++ src/sublime.py
@@ -1,43 +1,45 @@
+# pylint: disable=C0103, W0311, C0301
+
 import os, sys
 import re, glob
 import xml.etree.ElementTree as ET
 
-projects = os.path.join(os.path.expanduser("~"),sys.argv[1])
+projects = os.path.join(os.path.expanduser("~"), sys.argv[1])
 items = []
 sites = [d for d in os.listdir(projects) if not d.startswith(".")]
 
-if (len(sys.argv) > 2):
-    sites = [s for s in sites if re.search(sys.argv[2],s)]
+if len(sys.argv) > 2:
+  sites = [s for s in sites if re.search(sys.argv[2], s)]
 
 for site in sites:
-    path = os.path.join(projects,site)
-    icon = glob.glob(path + "*/favicon.ico")
-    if len(icon):
-        icon = icon[0]
-    else:
-        icon = "icon.png"
-    items.append({'uid': path,
-                  'arg': path,
-                  'title': site,
-                  'subtitle': path,
-                  'icon': icon,
-                  'valid': "yes"})
+  path = os.path.join(projects, site)
+  icon = glob.glob(path + "*/favicon.ico")
+  if len(icon):
+    icon = icon[0]
+  else:
+    icon = "icon.png"
+  items.append({'uid': path,
+                'arg': path,
+                'title': site,
+                'subtitle': path,
+                'icon': icon,
+                'valid': "yes"})
 
 if not len(items):
-    items = [{'uid': "none",
-              'arg': "arg",
-              'title': "No Sites Found",
-              'subtitle': "No sites matching your query were found",
-              'icon': "icon.png",
-              'valid': "no"}]
+  items = [{'uid': "none",
+            'arg': "arg",
+            'title': "No Sites Found",
+            'subtitle': "No sites matching your query were found",
+            'icon': "icon.png",
+            'valid': "no"}]
 
 xml = ET.Element("items")
 
 for item in items:
-    xml_item = ET.SubElement(xml,"item",{'uid':item['uid'],'arg':item['arg']})
-    xml_title =  ET.SubElement(xml_item,"title").text = item['title']
-    xml_subtitle =  ET.SubElement(xml_item,"subtitle").text = item['subtitle']
-    xml_icon =  ET.SubElement(xml_item,"icon").text = item['icon']
-    xml_valid =  ET.SubElement(xml_item,"valid").text = item['valid']
+  xml_item = ET.SubElement(xml, "item", {'uid': item['uid'], 'arg': item['arg']})
+  xml_title = ET.SubElement(xml_item, "title").text = item['title']
+  xml_subtitle = ET.SubElement(xml_item, "subtitle").text = item['subtitle']
+  xml_icon = ET.SubElement(xml_item, "icon").text = item['icon']
+  xml_valid = ET.SubElement(xml_item, "valid").text = item['valid']
 
 print ET.tostring(xml)
